88f93a661ec333c01511cad98662f1a39ba1978f
@Annabel_ <hydrant94@gmail.com>
2023-09-29 17:16:16 +0200

    Initial Commit

---
+++ contactList_SUI/contactList_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  contactList_SUIApp.swift
+//  contactList_SUI
+//
+//  Created by Аня Беликова on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct contactList_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
